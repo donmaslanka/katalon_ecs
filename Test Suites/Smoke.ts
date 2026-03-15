701909b2f5dce6f58e10e8340361a658993aad71
Don Maslanka <dmaslanka@gmail.com>
2026-03-15 14:11:14 -0600

    Add files via upload

---
+++ Test Suites/Smoke.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+  <description></description>
+  <name>Smoke</name>
+  <tag></tag>
+  <isRerun>false</isRerun>
+  <mailRecipient></mailRecipient>
+  <numberOfRerun>0</numberOfRerun>
+  <pageLoadTimeout>30</pageLoadTimeout>
+  <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+  <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+  <rerunImmediately>false</rerunImmediately>
+  <testSuiteGuid>11111111-1111-1111-1111-111111111111</testSuiteGuid>
+  <testCaseLink>
+    <guid>22222222-2222-2222-2222-222222222222</guid>
+    <isReuseDriver>false</isReuseDriver>
+    <isRun>true</isRun>
+    <testCaseId>Test Cases/Smoke/TC_Jenkins2_Login_Reachable</testCaseId>
+    <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+  </testCaseLink>
+</TestSuiteEntity>
