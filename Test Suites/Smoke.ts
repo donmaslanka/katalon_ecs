ba00c14dd6350f10b63ba63b29c314bef07ccf27
Don Maslanka <dmaslanka@gmail.com>
2026-03-15 15:06:30 -0600

    Add files via upload

--- Test Suites/Smoke.ts
+++ Test Suites/Smoke.ts
@@ -1,21 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-  <description></description>
-  <name>Smoke</name>
-  <tag></tag>
-  <isRerun>false</isRerun>
-  <mailRecipient></mailRecipient>
-  <numberOfRerun>0</numberOfRerun>
-  <pageLoadTimeout>30</pageLoadTimeout>
-  <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-  <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-  <rerunImmediately>false</rerunImmediately>
-  <testSuiteGuid>11111111-1111-1111-1111-111111111111</testSuiteGuid>
-  <testCaseLink>
-    <guid>22222222-2222-2222-2222-222222222222</guid>
-    <isReuseDriver>false</isReuseDriver>
-    <isRun>true</isRun>
-    <testCaseId>Test Cases/Smoke/TC_Jenkins2_Login_Reachable</testCaseId>
-    <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-  </testCaseLink>
+   <description></description>
+   <name>Smoke</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cf80c8f6-6875-4b7a-8849-d3b00995e8b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>9e0f4a4c-1958-40c3-afec-42a831896a43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke/TC_Jenkins2_Login_Reachable</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
